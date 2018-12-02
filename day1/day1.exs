c8805dccb0ed5156743ddbd05ac8f77c06e86db5
Eleton <eleton@gmail.com>
2018-12-02 11:57:49 +0100

    Day 1_2 solved

--- day1/day1.exs
+++ day1/day1.exs
@@ -8,9 +8,32 @@ defmodule Day1 do
       |> Enum.map(fn v -> String.to_integer(v) end)
   end
 
+  def outerCalibrate(signal) do outerCalibrate(signal, 0, []) end
+
+  def outerCalibrate(signal, frequency, frequencies) do
+    case calibrate(signal, frequency, frequencies) do
+      {frequency, frequencies} -> outerCalibrate(signal, frequency, frequencies)
+      frequency -> frequency
+    end
+  end
+  def calibrate([], frequency, frequencies) do
+    {frequency, frequencies}
+  end
+  def calibrate([value | signal], frequency, frequencies) do
+    newFrequency = value + frequency
+    case Enum.member?(frequencies, newFrequency) do
+      true -> newFrequency
+      false -> calibrate(signal, newFrequency, [newFrequency | frequencies])
+    end
+  end
+
 end
 
 # Day1.decode([1,2,-6])
+# Day1.parseSignal("signal.txt")
+#   |> Day1.decode
+#   |> IO.inspect
+
 Day1.parseSignal("signal.txt")
-  |> Day1.decode
+  |> Day1.outerCalibrate
   |> IO.inspect
